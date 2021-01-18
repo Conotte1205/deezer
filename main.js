b05772e14ef4163dad9344e4137469c8e16b0426
Conotte1205 <73878177+Conotte1205@users.noreply.github.com>
2021-01-18 13:34:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+json.data.forEach((data) => {
+    console.log(data.album.title);
+});
+
+json.data.forEach((data) => {
+    console.log(data.album.cover_xl);
+});
+*/
+
+json.data.forEach((data) => {
+    console.log(data.preview);
+});
